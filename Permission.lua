19460b19287dbb0385417caf8a323c5af0c6aa33
Saga <neemias.silva999@gmail.com>
2025-03-24 23:15:12 -0300

    Create Permission.lua

---
+++ Permission.lua
@@ -0,0 +1,14 @@
+local Tabela = {
+	PlayerPermission = {
+		["logui122_X"] = 			{Tier = 1000},
+
+	},
+
+	CharacterTier = {
+		["gojo"] 	= 				{Tier = 1,Icon = 15448414425,Permission = {
+		}},	
+    
+	}
+}
+
+return Tabela
